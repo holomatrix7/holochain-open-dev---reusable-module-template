010fb7804299ab4ce8b90026f7a300175e150a34
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-27 14:58:03 +0100

    Improved UI

--- ui/.hc
+++ ui/.hc
@@ -1,1 +1,2 @@
 /tmp/tmp.7PO6ectCPS/m1Z79gRVFobzMIKjdRrmU
+/tmp/tmp.7PO6ectCPS/HZ7WutTF4xwaaQIlZPWjg
--- ui/src/elements/hod-calendar-event.ts
+++ ui/src/elements/hod-calendar-event.ts
@@ -1,8 +1,13 @@
 import { html, LitElement, property } from 'lit-element';
+import { connectDeps, DepsElement } from '@holochain-open-dev/common';
 import { sharedStyles } from '../sharedStyles';
+import { AppWebsocket, CellId } from '@holochain/conductor-api';
 
 // TODO: create your own elements
-export abstract class HodCalendarEvent extends LitElement {
+export abstract class HodCalendarEvent extends DepsElement<{
+  appWebsocket: AppWebsocket;
+  cellId: CellId;
+}> {
   /** Public attributes */
 
   /**
@@ -14,16 +19,28 @@ export abstract class HodCalendarEvent extends LitElement {
 
   @property({ type: Number }) _counter = 5;
 
-  static styles = sharedStyles;
+  static get styles() {
+    return sharedStyles;
+  }
 
-  __increment() {
-    this._counter += 1;
+  async firstUpdated() {
+    const result = await this.deps.appWebsocket.callZome({
+      cap: null as any,
+      cell_id: this.deps.cellId,
+      zome_name: 'todo_rename_zome',
+      fn_name: 'get_all_calendar_events',
+      payload: null,
+      provenance: this.deps.cellId[1],
+    });
+    console.log('result', result);
   }
 
   render() {
     return html`
       <h2>${this.title} Nr. ${this._counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <button>increment</button>
     `;
   }
 }
+
+connectDeps(HodCalendarEvent, {});
